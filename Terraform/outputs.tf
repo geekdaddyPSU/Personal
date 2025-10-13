6f9c5a5e0f8ea30ed3f9ff1b7a915515e83042d3
Ben Pitzer <bpitzer@gmail.com>
2025-10-13 14:15:38 -0400

    Committing initial terraform code for AWS ECS deployment

---
+++ Terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.main.dns_name
+}
+
+output "alb_url" {
+  description = "URL of the Application Load Balancer"
+  value       = "http://${aws_lb.main.dns_name}"
+}
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
+}
+
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.main.name
+}
+
+output "cloudwatch_log_group" {
+  description = "CloudWatch log group name"
+  value       = aws_cloudwatch_log_group.ecs.name
+}
